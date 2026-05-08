a058205c2397f551add0a40b3cb9ec6dd87f796a
OPL <rialo.lopes@gmail.com>
2026-05-07 22:20:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def _injetar_css():
         top: 0; left: 0; right: 0;
         z-index: 999999;
         height: 56px;
-        background: #0F6E56;
+        background: ##e6a50e;
         display: flex;
         align-items: center;
         padding: 0 20px;
